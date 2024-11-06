24cfebf7e741570c34d9bf4eb51c11b938f972d3
Rishabh Gusain <rishabhgusain.6@gmail.com>
2024-11-06 22:39:01 +0530

    commented awards and recognition section

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ function App() {
             </div>
             </section>
 
-          <section className="opacity-0 animate-fade-in" style={{animationDelay: '1.4s'}}>
+          {/* <section className="opacity-0 animate-fade-in" style={{animationDelay: '1.4s'}}>
             <h3 className="text-2xl font-semibold mb-6 tracking-wide">AWARDS AND RECOGNITIONS</h3>
             <div className="space-y-6">
               {awards.map((award, index) => (
@@ -103,7 +103,7 @@ function App() {
                 </div>
               ))}
             </div>
-          </section>
+          </section> */}
         </main>
       </div>
     </div>
